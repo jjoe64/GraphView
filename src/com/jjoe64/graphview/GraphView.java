ed002d6f7fde8e0442b492c73af7b524575caa6c
NMA <narendra_m_a@yahoo.com>
2012-04-05 23:59:44 +0530

    Removed Log import

--- src/com/jjoe64/graphview/GraphView.java
+++ src/com/jjoe64/graphview/GraphView.java
@@ -11,7 +11,6 @@ import android.graphics.Paint;
 import android.graphics.Paint.Align;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
