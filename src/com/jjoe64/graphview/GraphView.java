8b5907b8835068d5c24726fb949fcec4b18f9250
Jonas Gehring <g.jjoe64@googlemail.com>
2013-12-03 08:33:51 +0100

    realtime scrollToEnd does not clear labels width/height cache, so that it is not flickering

--- src/com/jjoe64/graphview/GraphView.java
+++ src/com/jjoe64/graphview/GraphView.java
@@ -759,6 +759,12 @@ abstract public class GraphView extends LinearLayout {
 		if (!scrollable) throw new IllegalStateException("This GraphView is not scrollable.");
 		double max = getMaxX(true);
 		viewportStart = max-viewportSize;
+
+		// don't clear labels width/height cache
+		// so that the display is not flickering
+		if (!staticVerticalLabels) verlabels = null;
+		if (!staticHorizontalLabels) horlabels = null;
+
 		invalidate();
 		viewVerLabels.invalidate();
 		graphViewContentView.invalidate();
