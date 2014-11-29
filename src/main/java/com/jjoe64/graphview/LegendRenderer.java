5c7b81997075f750edb06dac4d38fa2c27574a54
jogehring <jogehring@inovex.de>
2014-11-29 14:57:15 +0100

    license to GPL with linking exception

--- src/main/java/com/jjoe64/graphview/LegendRenderer.java
+++ src/main/java/com/jjoe64/graphview/LegendRenderer.java
@@ -1,3 +1,22 @@
+/**
+ * GraphView
+ * Copyright (C) 2014  Jonas Gehring
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * with the "Linking Exception", which can be found at the license.txt
+ * file in this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * with the "Linking Exception" along with this program; if not,
+ * write to the author Jonas Gehring <g.jjoe64@gmail.com>.
+ */
 package com.jjoe64.graphview;
 
 import android.content.res.TypedArray;
