1da37d659c5705233a8504cc3603f5a11ff95226
jjoe64 <g.jjoe64@gmail.com>
2016-10-22 11:08:42 +0200

    changed license to apache 2.0

--- src/main/java/com/jjoe64/graphview/helper/DateAsXAxisLabelFormatter.java
+++ src/main/java/com/jjoe64/graphview/helper/DateAsXAxisLabelFormatter.java
@@ -1,21 +1,18 @@
 /**
  * GraphView
- * Copyright (C) 2014  Jonas Gehring
+ * Copyright 2016 Jonas Gehring
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License,
- * with the "Linking Exception", which can be found at the license.txt
- * file in this program.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * with the "Linking Exception" along with this program; if not,
- * write to the author Jonas Gehring <g.jjoe64@gmail.com>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.jjoe64.graphview.helper;
 
